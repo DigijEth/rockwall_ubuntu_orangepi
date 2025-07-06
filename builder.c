6e59a73ce43b53d6136c40b7c281b6da17802a44
Digi J <fightwithmusicinc@gmail.com>
2025-07-06 09:33:25 -0700

    Update and rename build.sh to builder.c

--- builder.c
+++ builder.c
@@ -1,3 +1,8 @@
+// builder.c
+// Orange Pi 5 Plus Linux Kernel Builder with Mali G610 GPU Support
+// 
+// This file should be saved as: builder.c
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
